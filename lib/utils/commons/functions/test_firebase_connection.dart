b0e6c2dc098f9c68d1c35f6e83606cc521d76f81
vinh <vinhntse2002@gmail.com>
2024-10-01 15:26:19 +0700

    build(firebase) setup firebase

--- lib/utils/commons/functions/test_firebase_connection.dart
+++ lib/utils/commons/functions/test_firebase_connection.dart
@@ -1,54 +1,54 @@
-// // import 'package:movemate/firebase_options.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:movemate_staff/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-// Future<void> testFirebaseConnectionWithPhone(String phoneNumber) async {
-//   try {
-//     await FirebaseAuth.instance.verifyPhoneNumber(
-//       phoneNumber: phoneNumber,
-//       verificationCompleted: (PhoneAuthCredential credential) async {
-//         UserCredential userCredential =
-//             await FirebaseAuth.instance.signInWithCredential(credential);
-//         print('Kết nối với Firebase thành công: ${userCredential.user?.uid}');
+Future<void> testFirebaseConnectionWithPhone(String phoneNumber) async {
+  try {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        UserCredential userCredential =
+            await FirebaseAuth.instance.signInWithCredential(credential);
+        print('Kết nối với Firebase thành công: ${userCredential.user?.uid}');
 
-//         String? idToken = await userCredential.user?.getIdToken();
+        String? idToken = await userCredential.user?.getIdToken();
 
-//         if (idToken != null) {
-//           print('Firebase ID Token: $idToken');
-//         } else {
-//           print('Không thể lấy ID Token');
-//         }
-//       },
-//       verificationFailed: (FirebaseAuthException e) {
-//         print('Xác thực số điện thoại thất bại: ${e.message}');
-//       },
-//       codeSent: (String verificationId, int? resendToken) {
-//         print('Mã xác thực đã được gửi đến $phoneNumber');
-//       },
-//       codeAutoRetrievalTimeout: (String verificationId) {
-//         print('Thời gian lấy mã xác thực đã hết.');
-//       },
-//     );
-//   } catch (e) {
-//     print('Kết nối với Firebase thất bại: $e');
-//   }
-// }
+        if (idToken != null) {
+          print('Firebase ID Token: $idToken');
+        } else {
+          print('Không thể lấy ID Token');
+        }
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print('Xác thực số điện thoại thất bại: ${e.message}');
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        print('Mã xác thực đã được gửi đến $phoneNumber');
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        print('Thời gian lấy mã xác thực đã hết.');
+      },
+    );
+  } catch (e) {
+    print('Kết nối với Firebase thất bại: $e');
+  }
+}
 
-// Future<void> testFirebaseConnection() async {
-//   try {
-//     UserCredential userCredential =
-//         await FirebaseAuth.instance.signInAnonymously();
-//     print('Kết nối với Firebase thành công: ${userCredential.user?.uid}');
+Future<void> testFirebaseConnection() async {
+  try {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInAnonymously();
+    print('Kết nối với Firebase thành công: ${userCredential.user?.uid}');
 
-//     User? user = userCredential.user;
-//     String? idToken = await user?.getIdToken();
+    User? user = userCredential.user;
+    String? idToken = await user?.getIdToken();
 
-//     if (idToken != null) {
-//       print('Firebase ID Token: $idToken');
-//     } else {
-//       print('Không thể lấy ID Token');
-//     }
-//   } catch (e) {
-//     print('Kết nối với Firebase thất bại: $e');
-//   }
-// }
+    if (idToken != null) {
+      print('Firebase ID Token: $idToken');
+    } else {
+      print('Không thể lấy ID Token');
+    }
+  } catch (e) {
+    print('Kết nối với Firebase thất bại: $e');
+  }
+}
