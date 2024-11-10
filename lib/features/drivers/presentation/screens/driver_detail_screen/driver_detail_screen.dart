f99d181c352af0e4c6155a6ab806dfac7dda6af4
vinh <vinhntse2002@gmail.com>
2024-11-10 16:15:38 +0700

    feat(map-navigation):initial map (#57)

--- lib/features/drivers/presentation/screens/driver_detail_screen/driver_detail_screen.dart
+++ lib/features/drivers/presentation/screens/driver_detail_screen/driver_detail_screen.dart
@@ -30,7 +30,7 @@ class _DriverDetailScreenState extends State<DriverDetailScreen> {
 
   Widget recenterButton = const SizedBox.shrink();
   Widget instructionImage = const SizedBox.shrink();
-
+  bool _isNavigationStarted = false;
   RouteProgressEvent? routeProgressEvent;
 
   @override
@@ -99,6 +99,33 @@ class _DriverDetailScreenState extends State<DriverDetailScreen> {
     }
   }
 
+  // Thêm hàm để xử lý bắt đầu điều hướng
+  Future<void> _startNavigation() async {
+    if (_isMapReady) {
+      try {
+        setState(() {
+          _isNavigationStarted = true;
+        });
+        await _navigationController?.startNavigation();
+      } catch (e) {
+        print("Lỗi khi bắt đầu điều hướng: $e");
+        // Nếu có lỗi, đặt lại trạng thái
+        setState(() {
+          _isNavigationStarted = false;
+        });
+      }
+    }
+  }
+
+  // Thêm hàm để xử lý kết thúc điều hướng
+  void _stopNavigation() {
+    setState(() {
+      _isNavigationStarted = false;
+    });
+    _navigationController?.finishNavigation();
+    _buildInitialRoute(); // Xây dựng lại tuyến đường ban đầu
+  }
+
   @override
   Widget build(BuildContext context) {
     List<String> deliveryPointCoordinates = widget.job.deliveryPoint.split(',');
@@ -127,7 +154,6 @@ class _DriverDetailScreenState extends State<DriverDetailScreen> {
                         _buildInitialRoute(); // Build route khi map đã sẵn sàng
                       });
                     },
-                    
                     onRouteProgressChange:
                         (RouteProgressEvent routeProgressEvent) {
                       if (mounted) {
@@ -140,11 +166,23 @@ class _DriverDetailScreenState extends State<DriverDetailScreen> {
                       }
                     },
                   ),
+                  if (!_isNavigationStarted)
+                    DeliveryDetailsBottomSheet(
+                      job: widget.job,
+                    ),
                   Positioned(
-                    bottom: 0,
+                    bottom: _isNavigationStarted
+                        ? 20
+                        : 280, // Điều chỉnh vị trí dựa trên trạng thái điều hướng
                     left: 0,
                     right: 0,
                     child: BottomActionView(
+                      onStopNavigationCallback: () {
+                        setState(() {
+                          instructionImage = const SizedBox.shrink();
+                          routeProgressEvent = null;
+                        });
+                      },
                       recenterButton: recenterButton,
                       controller: _navigationController,
                       routeProgressEvent: routeProgressEvent,
@@ -160,45 +198,18 @@ class _DriverDetailScreenState extends State<DriverDetailScreen> {
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          FloatingActionButton(
-            onPressed: _isMapReady
-                ? () async {
-                    try {
-                      await _navigationController?.startNavigation();
-                    } catch (e) {
-                      print("Lỗi khi bắt đầu điều hướng: $e");
-                    }
-                  }
-                : null, // Disable nút khi map chưa sẵn sàng
-            child: const Icon(Icons.directions),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _isMapReady
-                ? () {
-                    _navigationController?.overview();
-                  }
-                : null,
-            child: const Icon(Icons.route),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _isMapReady
-                ? () {
-                    _navigationController?.recenter();
-                  }
-                : null,
-            child: const Icon(Icons.center_focus_strong),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _isMapReady
-                ? () {
-                    _navigationController?.finishNavigation();
-                  }
-                : null,
-            child: const Icon(Icons.close),
-          ),
+          if (!_isNavigationStarted)
+            FloatingActionButton(
+              onPressed: _isMapReady ? _startNavigation : null,
+              child: const Icon(Icons.directions),
+            ),
+          if (!_isNavigationStarted) const SizedBox(height: 10),
+          // Nút kết thúc điều hướng chỉ hiển thị khi đang trong chế độ điều hướng
+          if (_isNavigationStarted)
+            FloatingActionButton(
+              onPressed: _isMapReady ? _stopNavigation : null,
+              child: const Icon(Icons.close),
+            ),
         ],
       ),
     );
