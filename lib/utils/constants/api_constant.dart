540d5417ff6138204d3909aec46f0fc69f80a313
tuanpv62 <phantuan62.paster@gmail.com>
2024-12-17 06:18:39 +0700

    change key

--- lib/utils/constants/api_constant.dart
+++ lib/utils/constants/api_constant.dart
@@ -73,8 +73,11 @@ class APIConstants {
   // static const apiVietMapKey =
   //     "be00f7e132bdd086ccd57e21460209836f5d37ce56beaa42";
   // // api vietmap-key
-  static const apiVietMapKey =
-      "e7fb2f56a9eca6890aae01882c6b789527a21dcf88c75145";
+  // static const apiVietMapKey =
+  //     "e7fb2f56a9eca6890aae01882c6b789527a21dcf88c75145";
+
+   static const apiVietMapKey =
+      "38db2f3d058b34e0f52f067fe66a902830fac1a044e8d444";
 
   // error
   static const Map<String, String> errorTrans = {
